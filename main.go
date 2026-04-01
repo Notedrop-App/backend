434337eb46db1713a1e451f87ba890eb71f9453b
SkuliX01 <bartosz.krawczyk34@gmail.com>
2026-04-01 11:48:31 +0200

    Add: seperate functions to folders, add util function to load env file

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v3"
-	"github.com/joho/godotenv"
+	"github.com/notedrop-app/backend/utils"
 )
 
 func init() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	utils.LoadENV("./.env")
 }
 
 func main() {
